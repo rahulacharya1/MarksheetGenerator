04b9537d168ebd86cacdab122b6a6fdde5783ff1
rahulkumaracharya199-prog <rahulkumaracharya199@gmail.com>
2026-03-25 09:03:31 +0530

    feat: Update storage configuration to use Cloudinary and Whitenoise for static files

--- marksheet/marksheet/settings.py
+++ marksheet/marksheet/settings.py
@@ -122,6 +122,13 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': 'uhDUuJkgMxgiW9pWx6JaFI5jCv8',
 }
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 
